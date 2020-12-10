a9867c5a3d62209f317b27cd49cef797e36e01c8
DJcri <djcri123@gmail.com>
2020-12-09 19:09:21 -0800

    Added functionality to drop down button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+// my-script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const defNav = document.querySelector(".default-nav");
+  const navDropBtn = document.querySelector(".drop-down");
+  document.querySelector("body").onresize = function () {
+    if (window.innerWidth <= 850) {
+      defNav.classList.toggle("hidden");
+    } else {
+      defNav.classList.remove("hidden");
+    }
+  };
+  navDropBtn.addEventListener("click", function () {
+    defNav.classList.toggle("hidden");
+  });
+});
