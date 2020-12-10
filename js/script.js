5ba5024f191b643143b153cc6cc2e4475b1a0c1a
DJcri <djcri123@gmail.com>
2020-12-09 19:22:28 -0800

    Made drop down less buggy

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 "use strict";
-// my-script.js
+
 document.addEventListener("DOMContentLoaded", function () {
   const defNav = document.querySelector(".default-nav");
   const navDropBtn = document.querySelector(".drop-down");
   document.querySelector("body").onresize = function () {
-    if (window.innerWidth <= 850) {
-      defNav.classList.toggle("hidden");
-    } else {
+    if (window.innerWidth <= 850 && !defNav.classList.contains("hidden")) {
+      defNav.classList.add("hidden");
+    } else if (window.innerWidth > 850) {
       defNav.classList.remove("hidden");
     }
   };
